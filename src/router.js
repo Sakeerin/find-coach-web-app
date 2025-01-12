92e473c81ee0079545bd7b5d6a37523ba335857f
Sakeerin <theblue0800@gmail.com>
2025-01-12 11:34:08 +0700

    add route page component and main layout styling

--- src/router.js
+++ src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import CoachDetail from './pages/coaches/CoachesDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachesRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', component: null },
-        { path: '/coaches/:id', component: null, 
+        { path: '/coaches', component: CoachesList },
+        { path: '/coaches/:id', component: CoachDetail, 
             children: [
-                { path: 'contract', component: null } // coaches/id/contract
+                { path: 'contract', component: ContactCoach } // coaches/id/contract
             ],
         },    
-        { path: '/register', component: null },
-        { path: '/requests', component: null },
-        { path: '/:notFound(.*)', component: null },
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: RequestReceived },
+        { path: '/:notFound(.*)', component: NotFound },
     ],
 });
 
