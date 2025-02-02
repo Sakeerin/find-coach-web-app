01f1e6a80d658551f5f30b4a712c58a4fdf73e99
Sakeerin <theblue0800@gmail.com>
2025-02-02 11:29:13 +0700

    implement coaches list and list items

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,6 +7,51 @@
             <button>Refresh</button>
             <router-link to="/register">Register as Coach</router-link>
         </div>
-        <ul>LIST OF COACHES</ul>
+        <ul v-if="hasCoaches">
+            <coach-item 
+                v-for="coach in filteredCoaches" 
+                :key="coach.id" 
+                :id="coach.id" 
+                :first-name="coach.firstName"
+                :last-name="coach.lastName" 
+                :rate="coach.hourlyRate" 
+                :areas="coach.areas">
+            </coach-item>
+            <!-- <li v-for="coach in fileredCoaches" :key="coach.id">
+                {{ coach.firstName }}
+            </li> -->
+        </ul>
+        <h3 v-else>No Coaches found.</h3>
     </section>
 </template>
+
+<script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
+export default {
+    components: {
+        CoachItem
+    },
+    computed: {
+        filteredCoaches() {
+            return this.$store.getters['coaches/coaches']
+        },
+        hasCoaches() {
+            return this.$store.getters['coaches/hasCoaches'];
+        }
+    },
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
